aefd3c2ae502edb68d51cafde3e00857977583c3
IENGROUND <ericano.rhee@gmail.com>
2025-10-19 16:01:18 +0900

    feat(NoticeListScreen): Placeholder 추가

--- src/ui/screens/root/notice/list/NoticeListScreen.tsx
+++ src/ui/screens/root/notice/list/NoticeListScreen.tsx
@@ -2,15 +2,16 @@ import DefaultLayout from "../../../../utils/layout/DefaultLayout.tsx";
 import {CommonWrapper} from "../../../../utils/layout/CommonWrapper.tsx";
 import {BellRingingIcon, PushPinIcon} from "@phosphor-icons/react";
 import styled from "styled-components";
-import {Button, Card, Chip, Image} from "@heroui/react";
+import {Button, Card, Chip, Image, Skeleton} from "@heroui/react";
 import {useElementRefs, useVisibleAnimation} from "../../../../utils/utils.ts";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 import type {Notice} from "../../../../../data/notice/Notice.tsx";
 import {useNoticeListViewModel} from "./NoticeListViewModel.ts";
 import {useDateTimeFormat} from "../../../../utils/utils/DateTimeFormat.ts";
 import {useTranslation} from "react-i18next";
 import {useNavigate} from "react-router";
 import {NoticeDestination} from "../NoticeDestination.ts";
+import {CSSTransition} from "react-transition-group";
 
 export default function NoticeListScreen() {
   const { infoStateList, startListening, stopListening } = useNoticeListViewModel();
@@ -25,6 +26,9 @@ export default function NoticeListScreen() {
   const [visibleAnimationRefs, addToVisibleAnimationRefs, refCount] = useElementRefs<HTMLDivElement>();
   useVisibleAnimation(visibleAnimationRefs, "start", refCount);
 
+  const placeholderRef = useRef(null);
+  const listRef = useRef(null);
+
   return (
     <DefaultLayout>
       <CommonWrapper>
@@ -36,17 +40,34 @@ export default function NoticeListScreen() {
           // className="visible-animation"
           // ref={addToVisibleAnimationRefs}
         >
-          {
-            infoStateList.isInitialized ? (
-              infoStateList.itemList.map((item) => (
-                <NoticeCell item={item} onClick={() => navigate(`${NoticeDestination.route}?${item?.id}`)} />
-              ))
-            ) : (
-              <div>placeholder</div>
-            )
-          }
-
-
+          <CSSTransition
+            in={!infoStateList.isInitialized}
+            timeout={300}
+            classNames="fade"
+            nodeRef={placeholderRef}
+          >
+            <div ref={placeholderRef} className="flexbox">
+              {
+                Array(2).fill(null).map((_, index) => (
+                  <NoticeCellShimmer index={index} />
+                ))
+              }
+            </div>
+          </CSSTransition>
+          <CSSTransition
+            in={infoStateList.isInitialized}
+            timeout={300}
+            classNames="fade"
+            nodeRef={listRef}
+          >
+            <div ref={listRef} className="flexbox">
+              {
+                infoStateList.itemList.map((item) => (
+                  <NoticeCell key={item.id} item={item} onClick={() => navigate(`${NoticeDestination.route}?${item?.id}`)} />
+                ))
+              }
+            </div>
+          </CSSTransition>
         </ContentWrapper>
       </CommonWrapper>
     </DefaultLayout>
@@ -57,10 +78,35 @@ const ContentWrapper = styled.div`
   width: 100%;
   padding: 1rem;
   
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  align-items: center;
+  position: relative;
+  
+  .fade-enter {
+    opacity: 0;
+  }
+  /* 요소가 나타나는 동안의 상태 (애니메이션 적용) */
+  .fade-enter-active {
+    opacity: 1;
+    transition: opacity 0.3s ease-in-out;
+  }
+  /* 요소가 사라질 때의 최종 상태 */
+  .fade-exit-active {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+  
+  .fade-exit-done {
+    opacity: 0;
+  }
+  
+  .flexbox {
+    position: absolute;
+    width: 100%;
+    
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+  }
 `;
 
 const NoticeCellWrapper = styled(Card)`
@@ -104,6 +150,7 @@ const NoticeCellWrapper = styled(Card)`
     }
     
     & > .date {
+      margin-top: 0.5rem;
       text-align: start;
       font-size: small;
     }
@@ -137,7 +184,6 @@ const NoticeCell = ({ item, onClick }: { item: Notice, onClick: () => void }) =>
       disableRipple
       disableAnimation
       onPress={onClick}
-      key={item.id}
     >
       <div className="content">
         <div className="category">
@@ -167,6 +213,46 @@ const NoticeCell = ({ item, onClick }: { item: Notice, onClick: () => void }) =>
             /> : <></>
         }
       </div>
+    </NoticeCellWrapper>
+  )
+}
+
+const NoticeCellShimmer = ({ index }: { index: number }) => {
+  const { t } = useTranslation();
+  return (
+    <NoticeCellWrapper>
+      <div className="content">
+        <div className="category">
+          {
+            index % 2 === 0 ?
+              <Skeleton
+                // isLoaded
+                className="rounded-lg"
+              >
+                <Chip radius="sm" color="danger" startContent={<PushPinIcon size={18} weight="fill" />} variant="flat">
+                  {t("strings:fixed")}
+                </Chip>
+              </Skeleton>
+              : <></>
+          }
+          <Skeleton className="rounded-lg"><Chip radius="sm">카테고리</Chip></Skeleton>
+        </div>
+        <Skeleton className="title rounded-lg"><h3 className="title">제목</h3></Skeleton>
+        <Skeleton className="date rounded-lg"><div className="date">시간</div></Skeleton>
+      </div>
+      {
+        index % 2 === 0 ?
+          <div className="end-content">
+            <Skeleton
+              className="rounded-lg"
+              style={{
+                width: "100%",
+                height: "100%",
+                aspectRatio: 1
+              }}
+            />
+          </div>: <></>
+        }
     </NoticeCellWrapper>
   )
 }
