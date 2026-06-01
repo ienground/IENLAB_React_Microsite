07ccb79f5df6ac57e9431a5597d0291811618c74
IENGROUND <my@ien.zone>
2026-06-02 02:28:51 +0900

    ✨ feat(core): Firebase 환경 변수 이름 변경 및 GitHub Actions 워크플로우 추가
    - FirebaseConfig.ts에서 환경 변수 이름을 VITE_APP_에서 VITE_로 변경
    - .firebaserc 파일에 admin 권한 설정 추가
    - 개발 브랜치용 Firebase preview 워크플로우 추가
    - 메인 브랜치용 Firebase 배포 워크플로우 추가

--- src/constant/FirebaseConfig.ts
+++ src/constant/FirebaseConfig.ts
@@ -9,13 +9,13 @@ import {getStorage} from "firebase/storage"
 import {connectFunctionsEmulator, getFunctions} from "firebase/functions"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
